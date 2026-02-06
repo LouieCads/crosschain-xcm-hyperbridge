38b5a03590db044f7ba40aee6518425f21bf9b86
Louie <louigiecads143@gmail.com>
2026-02-07 06:16:11 +0800

    feat: add deployment script and deploy to hardhat

--- contracts/ignition/modules/TokenBridge.ts
+++ contracts/ignition/modules/TokenBridge.ts
@@ -1,7 +1,21 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import { parseEther } from "viem";
 
-// export default buildModule("TokenBridgeModule", (m) => {
-//   const counter = m.contract("TokenBridge");
+export default buildModule("TokenBridgeModule", (m) => {
+  const tokenGatewayAddress = m.getParameter(
+    "tokenGateway",
+    "0x0000000000000000000000000000000000000000" 
+  );
+  
+  const feeTokenAddress = m.getParameter(
+    "feeToken",
+    "0x0000000000000000000000000000000000000000" 
+  );
 
-//   return { counter };
-// });
+  const tokenBridge = m.contract("TokenBridge", [
+    tokenGatewayAddress,
+    feeTokenAddress,
+  ]);
+
+  return { tokenBridge };
+});
