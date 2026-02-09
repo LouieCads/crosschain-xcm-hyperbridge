a25e960b0b078ae7ec157d152f7334241e216572
Louie <louigiecads143@gmail.com>
2026-02-09 10:27:27 +0800

    feat: add token bridge ABI and localhost contract address

---
+++ web/src/lib/contracts.ts
@@ -0,0 +1,201 @@
+/**
+ * Contract ABI 
+ */
+export const TOKEN_BRIDGE_ABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_tokenGateway",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "_feeToken",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "InsufficientAllowance",
+      "type": "error"
+    },
+    {
+      "inputs": [],
+      "name": "InsufficientAmount",
+      "type": "error"
+    },
+    {
+      "inputs": [],
+      "name": "InsufficientBalance",
+      "type": "error"
+    },
+    {
+      "inputs": [],
+      "name": "InvalidRecipient",
+      "type": "error"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "token",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "sender",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "bytes32",
+          "name": "assetId",
+          "type": "bytes32"
+        },
+        {
+          "indexed": false,
+          "internalType": "bytes32",
+          "name": "recipient",
+          "type": "bytes32"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "bytes",
+          "name": "destChain",
+          "type": "bytes"
+        }
+      ],
+      "name": "BridgeInitiated",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "token",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address",
+          "name": "recipient",
+          "type": "address"
+        },
+        {
+          "internalType": "bytes",
+          "name": "destChain",
+          "type": "bytes"
+        },
+        {
+          "internalType": "bool",
+          "name": "redeem",
+          "type": "bool"
+        },
+        {
+          "internalType": "uint256",
+          "name": "relayerFee",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint64",
+          "name": "timeout",
+          "type": "uint64"
+        }
+      ],
+      "name": "bridgeTokens",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "token",
+          "type": "address"
+        }
+      ],
+      "name": "deriveAssetId",
+      "outputs": [
+        {
+          "internalType": "bytes32",
+          "name": "",
+          "type": "bytes32"
+        }
+      ],
+      "stateMutability": "pure",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "feeToken",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "tokenGateway",
+      "outputs": [
+        {
+          "internalType": "contract ITokenGateway",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ] as const;
+
+  
+/**
+ * Contract Addresses 
+ */
+export const TOKEN_BRIDGE_ADDRESSES = {
+  // Hardhat localhost network (chain ID: 31337)
+  hardhat: '0x5FbDB2315678afecb367f032d93F642f64180aa3' as `0x${string}`,
+  // Sepolia testnet (chain ID: 11155111)
+  // sepolia: '' as `0x${string}`,
+  // Amoy testnet (chain ID: 80002)
+  // amoy: '' as `0x${string}`,
+} as const;
+
+/**
+ * Get contract address for a given chain ID
+ */
+export function getContractAddress(chainId: number): `0x${string}` | undefined {
+  switch (chainId) {
+    case 31337: // Hardhat localhost
+      return TOKEN_BRIDGE_ADDRESSES.hardhat;
+    // case 11155111: // Sepolia
+    //   return TOKEN_BRIDGE_ADDRESSES.sepolia;
+    // case 80002: // Amoy
+    //   return TOKEN_BRIDGE_ADDRESSES.amoy;
+    default:
+      return undefined;
+  }
+}
